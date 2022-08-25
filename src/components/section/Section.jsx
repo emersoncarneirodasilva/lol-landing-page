c7ea8cabb4f8dd7ae067b14056b264b75b0cb10d
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-08-25 12:22:57 -0300

    Created Repository

---
+++ src/components/section/Section.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './section.scss';
+
+const Section = props => {
+  return (
+    <div className={`section ${props.className}`}>
+      {props.children}
+    </div>
+  )
+}
+
+export const SectionContent = props => {
+  const bgImage = props.bgImage ? {
+    backgroundImage: `url(${props.bgImage})`
+  } : {}
+
+  return (
+    <div className={`section__content ${props.className}`} style={bgImage}>
+      {props.children}
+    </div>
+  )
+}
+
+export default Section;
